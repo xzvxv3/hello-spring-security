5c7837943ca554a2d6951485dfe4e14c136d4002
lgh02 <xzvxv3@gmail.com>
2026-06-01 18:31:12 +0900

    feat: ProductDto 수정 폼 유효성 검증 어노테이션 추가

--- src/main/java/kr/ac/hansung/dto/ProductDto.java
+++ src/main/java/kr/ac/hansung/dto/ProductDto.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +9,14 @@ import lombok.Setter;
 @Setter
 public class ProductDto {
 
+    @NotBlank
     private String name;
+
+    @Min(0)
     private int price;
+
     private String description;
+
+    @Min(0)
     private int stock;
 }
